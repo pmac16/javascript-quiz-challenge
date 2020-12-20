f59f737b15cc7d9dad32000df0ec4db9d8e60c64
Priya Macpherson <priyamacpherson@Priyas-MacBook-Pro.local>
2020-12-20 15:22:46 -0800

    added guiz files

---
+++ assets/js/quiz.js
@@ -0,0 +1,24 @@
+//Global Variables
+
+//display quiz question
+var questionContainer = document.createElement("div");
+questionContainer.className = "question";
+questionContainer.id = "question-container";
+
+var startPage = document.createElement("div");
+startPage.className = "start-page";
+startPage.innerHTML="<h1>JavaScript Challenge</h1>";
+masterContainer.appendChild(startPage);
+
+var directions = document.createElement("div");
+directions.className = "directions";
+directions.innerHTML="<h2>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!</h2>";
+masterContainer.appendChild(directions);
+
+var startButton = document.createElement("button");
+startButton.className = "container startButton";
+startButton.innerHTML = "Start Quiz"
+startButton.onclick = function() {startQuiz()}; 
+masterContainer.appendChild(startButton);
+
+document.body.appendChild(masterContainer);
